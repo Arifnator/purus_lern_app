e2d23ffdef1c7596749101ca673b8d8e0647d730
Arifnator <arifayduran@icloud.com>
2024-04-25 20:20:01 +0200

    2

---
+++ TestSplash.dart
@@ -0,0 +1,15 @@
+Text(
+'Anmelden',
+textAlign: TextAlign.center,
+style: TextStyle(
+color: Colors.white,
+fontSize: 21,
+fontFamily: 'SF Pro',
+fontWeight: FontWeight.w510,
+height: 0,
+),
+)
+// ---
+Text(
+'Bitte melden Sie sich an.',
+textAlign: TextAlign.center,
