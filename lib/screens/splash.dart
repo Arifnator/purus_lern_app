618bc8dc0690f46a61938f72359eb6e43436b901
Arif Ayduran <167970829+Arifnator@users.noreply.github.com>
2024-07-09 13:20:11 +0300

    ss

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:purus_lern_app/screens/dashboard.dart';
+// import 'package:purus_lern_app/screens/dashboard.dart';
 import 'package:purus_lern_app/screens/auth/login.dart';
 import 'package:flutter/material.dart';
 import 'package:purus_lern_app/styles/my_colors.dart';
