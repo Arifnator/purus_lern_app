05a72730e5cbbd16e196ba2a5002369976313bb1
Arif Ayduran <167970829+Arifnator@users.noreply.github.com>
2024-09-18 12:05:07 +0200

    temp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:purus_lern_app/main.dart';
+import 'package:purus_lern_app/oldmain.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
