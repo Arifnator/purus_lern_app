685f854310629e845cfee7d0a73705f9c59949d6
Arif Ayduran <167970829+Arifnator@users.noreply.github.com>
2024-09-19 09:46:15 +0200

    firestore übung

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,122 @@
+// import 'src/core/splash_screen.dart'; -- SplashScreen()
+// import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
+// import 'package:flutter_native_splash/flutter_native_splash.dart';
+// import 'package:purus_lern_app/ablage/tween_home.dart';
+// import 'package:window_manager/window_manager.dart';
+// import 'package:flutter/services.dart';
+
+import 'package:purus_lern_app/src/config/main_config.dart';
+import 'package:purus_lern_app/src/core/firebase/initialize_firebase.dart';
+import 'package:purus_lern_app/src/core/presentation/rive_manager.dart';
+import 'package:purus_lern_app/src/features/authentication/presentation/splash_gradient_animation.dart';
+
+// import 'package:rive_splash_screen/rive_splash_screen.dart';
+// import 'package:purus_lern_app/src/core/presentation/onboarding.dart';
+// import 'package:purus_lern_app/src/features/authentication/presentation/login_faceid_screen.dart';
+// import 'package:purus_lern_app/src/features/authentication/presentation/login_screen.dart';
+
+// feature first: https://mobterest.medium.com/mastering-feature-first-architecture-building-scalable-flutter-mobile-apps-5c706b6e42be
+
+// THEMEEEES
+
+// bottombar gradient animation
+
+// copy with her yerde oluyor mu?
+
+// CHeckbox widget
+
+// 1. First open + onboarding
+// 2. Autologin FaceID
+// 3. Angemeldet bleiben
+// flutter pub add sf_symbols // flutter pub add flutter_sficon  // flutter pub add drop_shadow_image
+
+// BackdropFilter(
+//                         filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+//                         child:
+
+// flutter pub add scaled_app -- runAppScaled
+
+//   smooth_corner: ^1.0.0
+
+// macos in firebase ekle
+
+// blur effekte
+
+// BOXCONTRAINS
+// rive ohne laden?
+
+// check internet connection? / offline learn??? -- https://www.youtube.com/watch?v=0si2HPnRCuE
+
+// Icons skalierbar!
+// icon & altina shadow effekt
+// ontap effekt
+// minimun size
+// main icon dunkelmodus
+// tüm snackbbar fln ios androit yap
+// alles skaliebar?
+// darkmode with splashsreen
 
 void main() async {
+  await initializeFirebase();
+
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  // // SplashScreen purusGreen:
+  // await Future.delayed(const Duration(milliseconds: 500));
+  // FlutterNativeSplash.remove();
+
+  // Initialize Rive and load animations
+  await RiveManager().initialize();
+
+  runApp(const PurusMain());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PurusMain extends StatelessWidget {
+  const PurusMain({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Hobbys',
-      home: HobbyPage(),
+      title: appName,
+      home: SplashGradientAnimation(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HobbyPage extends StatefulWidget {
-  const HobbyPage({super.key});
 
-  @override
-  State<HobbyPage> createState() => _HobbyPageState();
-}
 
-class _HobbyPageState extends State<HobbyPage> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _typeController = TextEditingController();
-  final TextEditingController _ratingController = TextEditingController();
 
-  Future<void> _addHobby() async {
-    await FirebaseFirestore.instance.collection('Hobbies').add({
-      'name': _nameController.text,
-      'type': _typeController.text,
-      'rating': _ratingController.text,
-    });
-    _nameController.clear();
-    _typeController.clear();
-    _ratingController.clear();
-  }
+// AnnotatedRegion<SystemUiOverlayStyle>(
+//             //Set status bar icon color
+//             value: your_condition
+//                 ? SystemUiOverlayStyle.dark.copyWith(
+//                     statusBarColor: your_color,
+//                     //statusBarIconBrightness: Brightness.light,
+//                   )
+//                 : SystemUiOverlayStyle.light.copyWith(
+//                     statusBarColor: your_color,
+//                     //statusBarIconBrightness: Brightness.light,
+//                   ),
+//             child:
+// ),
 
-  Stream<QuerySnapshot> _getHobbies() {
-    return FirebaseFirestore.instance.collection('Hobbies').snapshots();
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hobby Bewertungen'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(labelText: 'Hobby Name'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _typeController,
-              decoration: const InputDecoration(labelText: 'Hobby Art'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _ratingController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'Bewertung (0-5)'),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              int? rating = int.tryParse(_ratingController.text);
+//     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
 
-              if (rating != null && rating >= 0 && rating <= 5) {
-                _addHobby();
-              } else {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        content:
-                            const Text("Bitte Zahl zwischen 0 und 5 eingeben."),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: const Text("OK"),
-                          ),
-                        ],
-                      );
-                    });
-              }
-            },
-            child: const Text('Hobby hinzufügen'),
-          ),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: _getHobbies(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Center(
-                      child: Text('Fehler beim Abrufen der Daten'));
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
 
-                final data = snapshot.requireData;
 
-                return ListView.builder(
-                  itemCount: data.size,
-                  itemBuilder: (context, index) {
-                    var hobby = data.docs[index];
-                    return ListTile(
-                      title: Text(hobby['name']),
-                      subtitle: Text(
-                          'Art: ${hobby['type']} - Bewertung: ${hobby['rating']}'),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+
+
+
+  // async
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await windowManager.ensureInitialized();
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await windowManager.ensureInitialized();
+  // if (Platform.isMacOS) {
+  //   WindowManager.instance.setMinimumSize(const Size(20, 20));
+  // }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,7 +4,9 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   cloud_firestore
+  file_selector_windows
   firebase_core
+  firebase_storage
   local_auth_windows
   rive_common
   screen_retriever
