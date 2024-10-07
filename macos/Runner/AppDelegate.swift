61cf7948dbcf5372d9cd14275fb338c3919d40a9
Arif Ayduran <167970829+Arifnator@users.noreply.github.com>
2024-10-07 22:45:01 +0200

    appdelegate ++secure

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,14 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+  
+  // Ensure the app terminates when the last window is closed
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+  
+  // Opt-in to secure state restoration
+  override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
 }
